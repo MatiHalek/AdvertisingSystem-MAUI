57f36316adcb023319b66139d7cf7367c8bae44c
Mateusz Marmuźniak <matimarmuzniak@gmail.com>
2023-12-01 01:02:03 +0100

    Add project files.

---
+++ Vistaaa/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Vistaaa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
