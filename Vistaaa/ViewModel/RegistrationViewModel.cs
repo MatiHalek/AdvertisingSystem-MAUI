3c201f4e9f6ae49c247ba49734790102863a4b68
Mateusz Marmuźniak <matimarmuzniak@gmail.com>
2023-12-29 03:18:02 +0100

    Added MVVM validation, improved admin panel, minor improvements in appearance

---
+++ Vistaaa/ViewModel/RegistrationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vistaaa.Classes;
+
+namespace Vistaaa.ViewModel
+{
+    public partial class RegistrationViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ValidatableObject<string> email = new();
+
+        public RegistrationViewModel()
+        {
+            Email.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "A username is required."
+            });
+        }
+        [RelayCommand]
+        void ValidateEmail()
+        {
+            Email.Validate();
+            Debug.WriteLine("test" + Email.Value);
+        }
+    }
+}
